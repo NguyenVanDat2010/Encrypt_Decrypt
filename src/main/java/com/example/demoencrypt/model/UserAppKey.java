d109db47552960b2cafb2120b2a255c8fd428d84
nvdat <datdhvinh@gmail.com>
2025-06-22 15:56:20 +0700

    update code

--- src/main/java/com/example/demoencrypt/model/UserAppKey.java
+++
@@ -1,5 +0,0 @@
-//package com.example.demoencrypt.model;
-//
-//public class UserAppKey {
-////    private Long
-//}
--- src/main/java/com/example/demoencrypt/utils/DecryptData.java
+++ src/main/java/com/example/demoencrypt/utils/DecryptData.java
@@ -6,18 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.io.FileInputStream;
-import java.security.InvalidKeyException;
 import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.Arrays;
 import java.util.Base64;
 
 @Component
@@ -39,27 +31,14 @@ public class DecryptData {
     public String decryptData(String username) {
         try {
             byte[] bytes = Base64.getDecoder().decode(privateKey);
-//            System.out.println(bytes);
         // Tạo private key
             PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(bytes);
-
-//            System.out.println(privateKey.getBytes());
-
-
-            // Đọc file chứa private key
-//            FileInputStream fis = new FileInputStream("D:\\CRYPPT\\demo1\\src\\com\\demo\\RSA/privateKey.txt");
-//            byte[] b = new byte[fis.available()];
-//            fis.read(b);
-//            fis.close();
-//
-//            // Tạo private key
-//            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(b);
 
             KeyFactory factory = KeyFactory.getInstance("RSA");
             PrivateKey privKey = factory.generatePrivate(spec);
 
             // Giải mã dữ liệu
-            Cipher cipher = Cipher.getInstance("RSA");
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
             cipher.init(Cipher.DECRYPT_MODE, privKey);
 
             byte[] decryptOut = cipher.doFinal(Base64.getDecoder().decode(username));
